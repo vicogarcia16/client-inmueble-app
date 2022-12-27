fb542f4de59148c3ec1201f71d98e995699fcccc
vmgn16 <mg_619@hotmail.com>
2022-12-27 16:16:05 -0600

    Archivos directive

--- src/app/shared/popups/files-upload/files-upload.module.ts
+++ src/app/shared/popups/files-upload/files-upload.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilesUploadComponent } from './files-upload.component';
-
+import { FilesUploadDirective } from './files-upload.directive';
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 @NgModule({
   declarations: [
-    FilesUploadComponent
+    FilesUploadComponent,
+    FilesUploadDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatDialogModule
+  ],
+  exports: [
+    FilesUploadDirective
   ]
 })
 export class FilesUploadModule { }
