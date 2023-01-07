ebff71deebd4d79deade46d0dec11a0dc3c4a2af
vmgn16 <mg_619@hotmail.com>
2023-01-06 22:34:43 -0600

    Agregado subida tamaño de archivo

--- src/app/shared/popups/files-upload/files-upload.module.ts
+++ src/app/shared/popups/files-upload/files-upload.module.ts
@@ -5,6 +5,7 @@ import { FilesUploadDirective } from './files-upload.directive';
 import { MatDialogModule } from '@angular/material/dialog';
 import { DropZoneDirective } from './directives/drop-zone/drop-zone.directive';
 import { UploadComponent } from './components/upload/upload.component';
+import { FileSizePipe } from './pipes/file-size/file-size.pipe';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { UploadComponent } from './components/upload/upload.component';
     FilesUploadComponent,
     FilesUploadDirective,
     DropZoneDirective,
-    UploadComponent
+    UploadComponent,
+    FileSizePipe
   ],
   imports: [
     CommonModule,
